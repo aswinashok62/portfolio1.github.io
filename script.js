c72a11443d49ebe6fb6c72252cd831c361249831
aswinashok62 <143902263+aswinashok62@users.noreply.github.com>
2025-05-10 00:22:00 +0530

    script.js

--- script.js
+++ script.js
@@ -22,3 +22,29 @@ document.onload = () => {
     document.querySelector('.header').classList.remove('active');
   }
 };
+const navbar = document.querySelector('.header .navbar');
+const menuButton = document.querySelector('.header .menu');
+
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+  menuButton.classList.toggle('active'); // Optional: Add animation to the menu button as well
+});
+
+document.onscroll = () => {
+  navbar.classList.remove('show');
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+// When the page is loaded, check scroll position
+document.onload = () => {
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
